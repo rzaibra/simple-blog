bffbf3cf77b468c0f34d1eacbc03845bc057085a
rzaibra <rezaibra1559@gmail.com>
2016-03-04 20:46:47 +0700

    Tentang Saya

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Simple Blog</title>
+        <title>Tentang saya</title>
         <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" >
         <link rel="stylesheet" href="{{asset('assets/css/starter-template.css')}}" >
     </head>
